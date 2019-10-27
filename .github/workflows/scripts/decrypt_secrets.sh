ef18446fb22365a71cedd1c674e066adfb9a32ec
Brandon Ward <brandon.ward1993@gmail.com>
2019-10-27 10:44:37 -0600

    Fixing release build

--- .github/workflows/scripts/decrypt_secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Decrypt secret files.
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSWORD" \
---output $GITHUB_WORKSPACE/android/key.jks $GITHUB_WORKSPACE/android/key.jks.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSWORD" \
---output $GITHUB_WORKSPACE/android/key.properties $GITHUB_WORKSPACE/android/key.properties.gpg
